46630c188642e8a5b8a15a9995a83b4f03ebc499
Harlan Heath <HarlanDHeath@gmail.com>
2018-08-14 14:30:06 -0500

    update 1

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { json } = require("body-parser");
+const controller = require("./controller");
+
+const port = 3001;
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
